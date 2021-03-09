e200c914a810dab70c9e648793926dfc998605b9
qinzhengke <qinzhengke>
2021-03-09 14:51:20 +0800

    [NEW]Add example about thread.

--- src/container/map.cc
+++ src/container/map.cc
@@ -42,4 +42,22 @@ int main(int argc, char ** argv){
     for(auto p:m){cout<<"{"<<p.first<<","<<p.second<<"}"<<",";}
     cout<<endl;
 
+    std::map<int, std::string> c = {{1, "one"}, {2, "two"}, {3, "three"},
+                                    {4, "four"}, {5, "five"}, {6, "six"}};
+ 
+    // Erase some elements from the map
+    // Different from vector, after erasing one object, the iterator will not be
+    // assigned to next element automatically, you should use return value of 
+    // erase() to mannually reassign it.
+    for(auto it = c.begin(); it != c.end(); ) {
+        if(it->first % 2 == 1)
+            it = c.erase(it);   // Very important
+        else
+            ++it;
+    }
+ 
+    for(auto& p : c) {
+        std::cout << p.second << ' ';
+    }
+
 }
